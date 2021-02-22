be9e4e50df4d20982a8f9d16ae30c0f932512090
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-02-22 10:50:41 +0100

    Setup GTK 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate gtk;
+extern crate gio;
+
+use gtk::prelude::*;
+use gio::prelude::*;
+
+use gtk::{Application, ApplicationWindow, Button};
+
 fn main() {
-    println!("Hello, world!");
+    let application = Application::new(
+        Some("com.loloof64.desktop.chess_exercises_organizer"),
+        Default::default(),
+    ).expect("Failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("Chess exercises organizer");
+        window.set_default_size(350, 70);
+
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+        window.add(&button);
+
+        window.show_all();
+    });
+
+    application.run(&[]);
 }
